57af5813bc552d473e38023a8cd823ed910b5dbf
André Santos <andrerfcsantos@gmail.com>
2015-06-04 11:25:29 +0100

    Alterações Paginador - Remoção de excepção até ser encontrada melhor forma de colocar excepções na classe. - Mais métodos ncessários adicionados

--- src/lei/li3/g50/utilitarios/Paginador.java
+++ src/lei/li3/g50/utilitarios/Paginador.java
@@ -2,7 +2,6 @@ package lei.li3.g50.utilitarios;
 
 import java.util.List;
 import java.util.Objects;
-import lei.li3.g50.excepcoes.PaginaImpossivelException;
 
 public final class Paginador<E extends List<?>> {
 
@@ -10,155 +9,150 @@ public final class Paginador<E extends List<?>> {
     public static final int PAGINA_IMPOSSIVEL = -1;
 
     private E lista;
-    private int num_pag;
-    private int posicao_inicial;
-    private int num_elems_pag;
-    private int elems_por_pag;
+    private int numPaginaActual;
+    private int posInicialPagActual;
+    private int numElemsPagActual;
+    private int numElemsPorPag;
 
     /*
      CONSTRUCTORES
      */
-    private Paginador() {}
+    private Paginador() {
+    }
 
-    public Paginador(List<?> lista) throws PaginaImpossivelException {
+    public Paginador(List<?> lista) {
         if (lista == null) {
             throw new NullPointerException("Tentativa de inicializar paginador com array nulo.");
         }
         this.lista = (E) lista;
-        this.elems_por_pag = ELEMENTOS_POR_PAG_DEFAULT;
-        if (this.pagina_existe(1)) {
-            this.goto_pagina(1);
+        this.numElemsPorPag = ELEMENTOS_POR_PAG_DEFAULT;
+        if (this.paginaExiste(1)) {
+            this.gotoPagina(1);
         }
     }
 
-    public Paginador(List<?> lista, int num_elemetos_por_pag, int pag) throws PaginaImpossivelException {
+    public Paginador(List<?> lista, int num_elemetos_por_pag, int pag) {
         if (lista == null) {
             throw new NullPointerException("Tentativa de inicializar paginador com array nulo.");
         }
         this.lista = (E) lista;
-        this.elems_por_pag = (num_elemetos_por_pag > 0) ? num_elemetos_por_pag : ELEMENTOS_POR_PAG_DEFAULT;
-        if (this.pagina_existe(pag)) {
-            this.goto_pagina(pag);
+        this.numElemsPorPag = (num_elemetos_por_pag > 0) ? num_elemetos_por_pag : ELEMENTOS_POR_PAG_DEFAULT;
+        if (this.paginaExiste(pag)) {
+            this.gotoPagina(pag);
         }
     }
 
     public Paginador(Paginador pag) {
         this.lista = (E) pag.getLista();
-        this.num_pag = pag.getNumPag();
-        this.posicao_inicial = pag.getPosInicial();
-        this.num_elems_pag = pag.getNumElemsPag();
-        this.elems_por_pag = pag.getElemsPorPag();
+        this.numPaginaActual = pag.getNumPaginaActual();
+        this.posInicialPagActual = pag.getPosInicialPagActual();
+        this.numElemsPagActual = pag.getNumElemsPagActual();
+        this.numElemsPorPag = pag.getNumElemsPorPag();
     }
 
     /*
      GETTERS
      */
-    public static int getELEMENTOS_POR_PAG_DEFAULT() {
-        return ELEMENTOS_POR_PAG_DEFAULT;
-    }
-
-    public static int getPAGINA_IMPOSSIVEL() {
-        return PAGINA_IMPOSSIVEL;
-    }
 
     public E getLista() {
         return lista;
     }
 
-    public int getNumPag() {
-        return num_pag;
+    public int getNumPaginaActual() {
+        return numPaginaActual;
     }
 
-    public int getPosInicial() {
-        return posicao_inicial;
+    public int getPosInicialPagActual() {
+        return posInicialPagActual;
     }
 
-    public int getNumElemsPag() {
-        return num_elems_pag;
+    public int getNumElemsPagActual() {
+        return numElemsPagActual;
+    }
+    
+    public int getNumElems(){
+        return lista.size();
     }
 
-    public int getElemsPorPag() {
-        return elems_por_pag;
+    public int getNumElemsPorPag() {
+        return numElemsPorPag;
+    }
+    
+    public int getNumPaginas() {
+        int num_elems = this.lista.size();
+        int elems_por_pag = this.numElemsPorPag;
+        return ((num_elems % elems_por_pag) == 0) ? num_elems / elems_por_pag : (num_elems / elems_por_pag) + 1;
     }
 
     /*
      SETTERS
      */
     public void setLista(E lista) {
-        int nova_pagina = paginaDeIndice(this.posicao_inicial);
         this.lista = lista;
-        try {
-            this.goto_pagina(nova_pagina);
-        } catch (PaginaImpossivelException ex) {
-            
+        if (this.paginaExiste(this.numPaginaActual)) {
+            this.gotoPagina(this.numPaginaActual);
         }
     }
 
-    public void setElemsPorPag(int elems_por_pag) {
+    public void setNumElemsPorPag(int elems_por_pag) {
         int nova_pagina;
-        this.elems_por_pag = elems_por_pag;
-        nova_pagina = paginaDeIndice(this.posicao_inicial);
-
-        try {
-            this.goto_pagina(nova_pagina);
-        } catch (PaginaImpossivelException ex) {
-
+        this.numElemsPorPag = elems_por_pag;
+        nova_pagina = paginaDoIndice(this.posInicialPagActual);
+        if (this.paginaExiste(nova_pagina)) {
+            this.gotoPagina(nova_pagina);
         }
     }
 
     /*
      METODOS INSTANCIA
      */
-    public void goto_pagina(int numero_pagina) throws PaginaImpossivelException {
-        int comeco_pag = this.elems_por_pag * (numero_pagina - 1);
+    public void gotoPagina(int numero_pagina) {
+        int comeco_pag = this.numElemsPorPag * (numero_pagina - 1);
         int diferenca = this.lista.size() - comeco_pag;
 
         if (diferenca > 0 && numero_pagina > 0) {
-            this.posicao_inicial = comeco_pag;
-            this.num_elems_pag = (diferenca > this.elems_por_pag) ? this.elems_por_pag : diferenca;
-            this.num_pag = numero_pagina;
-        } else {
-            throw new PaginaImpossivelException();
+            this.posInicialPagActual = comeco_pag;
+            this.numElemsPagActual = (diferenca > this.numElemsPorPag) ? this.numElemsPorPag : diferenca;
+            this.numPaginaActual = numero_pagina;
         }
     }
 
-    public boolean pagina_existe() {
-        return pagina_existe(this.num_pag);
+    public boolean paginaExiste() {
+        return paginaExiste(this.numPaginaActual);
     }
 
-    public boolean pagina_existe(int num_pag) {
-        int comeco_pag = this.elems_por_pag * (num_pag - 1);
+    public boolean paginaExiste(int num_pag) {
+        int comeco_pag = this.numElemsPorPag * (num_pag - 1);
         int diferenca = this.lista.size() - comeco_pag;
 
         return diferenca > 0 && num_pag > 0;
     }
 
-    private int paginaDeIndice(int indice) {
-        double pag = indice / this.elems_por_pag;
+    private int paginaDoIndice(int indice) {
+        double pag = indice / this.numElemsPorPag;
         return (int) Math.floor(pag) + 1;
     }
-    
-    
-    /*
-    METODOS STANDARD
-    */
 
+    /*
+     METODOS STANDARD
+     */
     @Override
     public int hashCode() {
         int hash = 7;
         hash = 37 * hash + Objects.hashCode(this.lista);
-        hash = 37 * hash + this.num_pag;
-        hash = 37 * hash + this.posicao_inicial;
-        hash = 37 * hash + this.num_elems_pag;
-        hash = 37 * hash + this.elems_por_pag;
+        hash = 37 * hash + this.numPaginaActual;
+        hash = 37 * hash + this.posInicialPagActual;
+        hash = 37 * hash + this.numElemsPagActual;
+        hash = 37 * hash + this.numElemsPorPag;
         return hash;
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(this == obj)
+        if (this == obj) {
             return true;
-        
+        }
+
         if (obj == null) {
             return false;
         }
@@ -167,23 +161,22 @@ public final class Paginador<E extends List<?>> {
         }
         final Paginador<?> other = (Paginador<?>) obj;
         return this.lista == other.getLista()
-                && this.elems_por_pag == other.getElemsPorPag()
-                && this.num_elems_pag == other.getNumElemsPag()
-                && this.num_pag == other.getNumPag()
-                && this.posicao_inicial == other.getPosInicial();
+                && this.numElemsPorPag == other.getNumElemsPorPag()
+                && this.numElemsPagActual == other.getNumElemsPagActual()
+                && this.numPaginaActual == other.getNumPaginaActual()
+                && this.posInicialPagActual == other.getPosInicialPagActual();
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("Paginador{" + "tamanho lista=").append(lista.size()).append(", num_pag=").append(num_pag).append(", posicao_inicial=").append(posicao_inicial).append(", num_elems_pag=").append(num_elems_pag).append(", elems_por_pag=").append(elems_por_pag).append('}');
+        sb.append("Paginador{" + "tamanho lista=").append(lista.size()).append(", num_pag=").append(numPaginaActual).append(", posicao_inicial=").append(posInicialPagActual).append(", num_elems_pag=").append(numElemsPagActual).append(", elems_por_pag=").append(numElemsPorPag).append('}');
         return sb.toString();
     }
-    
+
     @Override
-    public Paginador clone(){
+    public Paginador clone() {
         return new Paginador(this);
     }
-    
-    
+
 }
