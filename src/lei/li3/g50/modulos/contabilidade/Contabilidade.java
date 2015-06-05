bf69fce88c3625049e3ee686cbfe18d41fedb924
André Santos <andrerfcsantos@gmail.com>
2015-06-05 21:34:25 +0100

    Estrutura para contabilidade

--- src/lei/li3/g50/modulos/contabilidade/Contabilidade.java
+++ src/lei/li3/g50/modulos/contabilidade/Contabilidade.java
@@ -1,92 +1,21 @@
 package lei.li3.g50.modulos.contabilidade;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.TreeMap;
+import lei.li3.g50.modulos.dados.Produto;
+import lei.li3.g50.utilitarios.Matriz_Double_12x2;
+import lei.li3.g50.utilitarios.Matriz_Int_12x2;
 
-import lei.li3.g50.modulos.dados.Mes;
 
 public class Contabilidade {
-
-	Map<Mes, ContabilidadeProdutoMes> contabilidade;
-
-	public Contabilidade() {
-		super();
-		this.contabilidade = new HashMap<Mes, ContabilidadeProdutoMes>();
-	}
-
-	public Contabilidade(Map<Mes, ContabilidadeProdutoMes> contabilidade) {
-		super();
-		this.contabilidade = new HashMap<Mes, ContabilidadeProdutoMes>();
-		this.setContabilidade(contabilidade);
-	}
-
-	public Contabilidade(Contabilidade contabilidade) {
-		super();
-		this.contabilidade = contabilidade.getContabilidade();
-	}
-
-	/**
-	 * @return the contabilidade
-	 */
-	public Map<Mes, ContabilidadeProdutoMes> getContabilidade() {
-		HashMap<Mes, ContabilidadeProdutoMes> tmp = new HashMap<Mes, ContabilidadeProdutoMes>();
-
-		for (Map.Entry<Mes, ContabilidadeProdutoMes> entry : this.contabilidade
-				.entrySet()) {
-			tmp.put(entry.getKey(), entry.getValue().clone());
-
-		}
-
-		return tmp;
-	}
-
-	/**
-	 * @param contabilidade
-	 *            the contabilidade to set
-	 */
-	public void setContabilidade(Map<Mes, ContabilidadeProdutoMes> contabilidade) {
-		for (Map.Entry<Mes, ContabilidadeProdutoMes> entry : contabilidade
-				.entrySet()) {
-			this.contabilidade.put(entry.getKey(), entry.getValue().clone());
-
-		}
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((contabilidade == null) ? 0 : contabilidade.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		Contabilidade other = (Contabilidade) obj;
-		if (contabilidade == null) {
-			if (other.getContabilidade() != null) {
-				return false;
-			}
-		} else if (!contabilidade.equals(other.getContabilidade())) {
-			return false;
-		}
-		return true;
-	}
-
-	public Contabilidade clone() {
-
-		return new Contabilidade(this);
-
-	}
+    
+    private Matriz_Int_12x2 totalComprasPorMes;
+    private Matriz_Int_12x2 totalUnidadesVendidasPorMes;
+    private Matriz_Double_12x2 totalFacturadoPorMes;
+    TreeMap<Produto, FichaProdutoContabilidade> arvoreProdutos;
+    
+    
+    public Contabilidade(){}
+    
+    
 
 }
---
+++ src/lei/li3/g50/modulos/contabilidade/FichaProdutoContabilidade.java
@@ -0,0 +1,16 @@
+package lei.li3.g50.modulos.contabilidade;
+
+import lei.li3.g50.utilitarios.Matriz_Double_12x2;
+import lei.li3.g50.utilitarios.Matriz_Int_12x2;
+
+public class FichaProdutoContabilidade {
+
+    private Matriz_Int_12x2 numUnidadesProdutoVendidasPorMes;
+    private Matriz_Int_12x2 numComprasProdutoPorMes;
+    private Matriz_Double_12x2 facturacaoProdutoPorMes;
+    
+    public FichaProdutoContabilidade(){}
+    
+    
+    
+}
