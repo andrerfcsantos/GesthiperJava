27b0e49f3703d2d794fd77b55a52419368a44799
André Santos <andrerfcsantos@gmail.com>
2015-07-11 15:05:48 +0100

    Correcção conflito merge - Eliminação do kryo na branch master, o kryo tem a sua branch propria.

--- src/lei/li3/g50/gesthiper/LeituraFicheiros.java
+++ src/lei/li3/g50/gesthiper/LeituraFicheiros.java
@@ -17,15 +17,12 @@ import lei.li3.g50.modulos.dados.Hipermercado;
 import lei.li3.g50.modulos.dados.Mes;
 import lei.li3.g50.modulos.dados.Produto;
 import lei.li3.g50.modulos.dados.TipoCompra;
-import lei.li3.g50.utilitarios.HipermercadoKryoSerializer;
 
-import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.io.Input;
-import com.esotericsoftware.kryo.io.Output;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class LeituraFicheiros {
 
-  
     public static void le_ficheiro_produtos(String str_ficheiro_produtos)
             throws FileNotFoundException, IOException {
         //FIXME var não usada
@@ -173,36 +170,19 @@ public class LeituraFicheiros {
     }
 
     public static void le_ficheiro_objecto(String str_ficheiro_objecto)
-			throws IOException, ClassNotFoundException {
+            throws IOException, ClassNotFoundException {
 
-		Kryo kryo = new Kryo();
-		kryo.register(Hipermercado.class, new HipermercadoKryoSerializer());
-	   
-	    
-	   
-		Input input = new Input(new FileInputStream(str_ficheiro_objecto));
-		Hipermercado someObject = (Hipermercado)kryo.readObject(input, Hipermercado.class);
-		Gesthiper.setHipermercado(someObject);
-		input.close();
-		// ObjectInputStream fich_obj = new ObjectInputStream(new
-		// FileInputStream(str_ficheiro_objecto));
-		// Gesthiper.setHipermercado((Hipermercado) fich_obj.readObject());
-		// fich_obj.close();
-	}
+        ObjectInputStream fich_obj = new ObjectInputStream(new FileInputStream(str_ficheiro_objecto));
+        Gesthiper.setHipermercado((Hipermercado) fich_obj.readObject());
+        fich_obj.close();
+    }
 
-	public static void guarda_ficheiro_objecto(String ficheiro)
-			throws FileNotFoundException, IOException {
-		Kryo kryo = new Kryo();
-		kryo.register(Hipermercado.class, new HipermercadoKryoSerializer());
-		Output output = new Output(new FileOutputStream(ficheiro));
+    public static void guarda_ficheiro_objecto(String ficheiro)
+            throws FileNotFoundException, IOException {
 
-		kryo.writeObject(output, Gesthiper.getHipermercado());
-		output.close();
-
-		// ObjectOutputStream out = new ObjectOutputStream(new
-		// FileOutputStream(ficheiro));
-		// out.writeObject(Gesthiper.getHipermercado());
-		// out.flush();
-		// out.close();
-	}
+        ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(ficheiro));
+        out.writeObject(Gesthiper.getHipermercado());
+        out.flush();
+        out.close();
+    }
 }
